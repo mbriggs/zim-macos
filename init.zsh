cac6424c4514eba6d198673035f8ca092509bcdc
Matt Briggs <matt@mattbriggs.net>
2021-03-09 02:26:41 -0500

    more

--- init.zsh
+++ init.zsh
@@ -1,30 +1,32 @@
-if [ ! -f /opt/homebrew/bin/brew ]
-then
-  return
-fi
-
-eval $(/opt/homebrew/bin/brew shellenv)
-
-alias ibrew='arch -x86_64 /usr/local/bin/brew'
-
-gnu=(export coreutils findutils gnu-indent gnu-sed gnutls grep gnu-tar gawk)
-
-for package in "${gnu[@]}"
-do
-  path=/opt/homebrew/opt/$package/libexec/gnubin
-  if [ ! -f "$path" ]
+() {
+  if [ ! -f /opt/homebrew/bin/brew ]
   then
-    /opt/homebrew/bin/brew install $package
+    return
   fi
-  export PATH=$path:$PATH
-done
 
-tools=(wget rsync curl ripgrep fd direnv git htop)
+  eval $(/opt/homebrew/bin/brew shellenv)
 
-for package in "${tools[@]}"
-do
-  if [ ! -f "/opt/homebrew/$package" ]
-  then
-    /opt/homebrew/bin/brew install $package
-  fi
-done
+  alias ibrew='arch -x86_64 /usr/local/bin/brew'
+
+  gnu=(export coreutils findutils gnu-indent gnu-sed gnutls grep gnu-tar gawk)
+
+  for package in "${gnu[@]}"
+  do
+    path=/opt/homebrew/opt/$package/libexec/gnubin
+    if [ ! -f "$path" ]
+    then
+      /opt/homebrew/bin/brew install $package
+    fi
+    export PATH=$path:$PATH
+  done
+
+  tools=(wget rsync curl ripgrep fd direnv git htop)
+
+  for package in "${tools[@]}"
+  do
+    if [ ! -f "/opt/homebrew/$package" ]
+    then
+      /opt/homebrew/bin/brew install $package
+    fi
+  done
+}
